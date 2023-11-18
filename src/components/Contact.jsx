87884d17b35ff1912486b2b7bd7945491f058acf
Satish_KG <satishkrgupta555@gmail.com>
2023-11-18 15:19:52 +0530

    "inputs nested inside 'label' tags"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,25 +7,40 @@ const Contact = () => {
             <form action='https://getform.io/f/fe0a60cc-3828-4bd4-a2b8-bd4dbf730968' method='POST' encType='multipart/form-data'>
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                     <div className='flex flex-col'>
-                        <label className='uppercase text-sm py-2'>Name</label>
-                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' autoComplete='on' name='name' placeholder='Enter your name'></input>
+                        <label className='uppercase text-sm py-2'>
+                            Name
+                            <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' autoComplete='on' name='name' placeholder='Enter your name'></input>
+                        </label>
+                        
                     </div>
                     <div className='flex flex-col'>
-                        <label className='uppercase text-sm py-2'>Phone</label>
-                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='phone' autoComplete='on' placeholder='Enter your phone number'></input>
+                        <label className='uppercase text-sm py-2'>
+                            Phone
+                            <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='phone' autoComplete='on' placeholder='Enter your phone number'></input>
+                        </label>
+                        
                     </div>
                 </div>
                 <div className='flex flex-col py-2'>
-                    <label className='uppercase text-sm py-2'>Email</label>
-                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type='email' name='email' autoComplete='on' placeholder='Enter your email'></input>
+                    <label className='uppercase text-sm py-2'>
+                        Email
+                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type='email' name='email' autoComplete='on' placeholder='Enter your email'></input>
+                    </label>
+                    
                 </div>
                 <div className='flex flex-col py-2'>
-                    <label className='uppercase text-sm py-2'>Subject</label>
-                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='sumject' placeholder='Enter subject of the mail'></input>
+                    <label className='uppercase text-sm py-2'>
+                        Subject
+                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='sumject' placeholder='Enter subject of the mail'></input>
+                    </label>
+                    
                 </div>
                 <div className='flex flex-col py-2'>
-                    <label className='uppercase text-sm py-2'>Message</label>
-                    <textarea className='border-2 rounded-lg p-3 flex border-gray-300' rows='10' name='message' placeholder='Enter your message'></textarea>
+                    <label className='uppercase text-sm py-2'>
+                        Message
+                        <textarea className='border-2 rounded-lg p-3 flex border-gray-300' rows='10' name='message' placeholder='Enter your message'></textarea>
+                    </label>
+                    
                 </div>
                 <button className='bg-[#001b5e] text-gray-100 mt-4 mb-4 w-full p-4 rounded-lg'>Send Message</button>
             </form>
