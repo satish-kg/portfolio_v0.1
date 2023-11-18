39267af18d327898cfd3efe4719a4bac0b96651d
Satish_KG <satishkrgupta555@gmail.com>
2023-11-18 15:37:13 +0530

    "sidenav changed to Sidenav in App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import Sidenav from './components/sidenav';
-// import Sidenav from './components/sidenav';
+import Sidenav from './components/Sidenav';
 import Main from './components/Main';
 import Work from './components/Work';
 import Projects from './components/Projects';
