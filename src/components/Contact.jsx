d0cfbd30a8e74ab1cb6494f86a71d777f08cc206
Satish_KG <satishkrgupta555@gmail.com>
2023-11-18 14:31:23 +0530

    "the site skeleton is complete. form is working"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className='max-w-[1040] m-auto p-4 py-16 md:pl-20'>
+        <div id='contact' className='max-w-[1040] m-auto p-4 py-16 md:pl-20'>
             <h1 className='py-4 text-center text-[#001b5e] pb-5 text-4xl font-bold'>Contact</h1>
-            <form action='' method='POST' encType='multipart/form-data'>
+            <form action='https://getform.io/f/fe0a60cc-3828-4bd4-a2b8-bd4dbf730968' method='POST' encType='multipart/form-data'>
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                     <div className='flex flex-col'>
                         <label className='uppercase text-sm py-2'>Name</label>
                         <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='name' placeholder='Enter your name'></input>
                     </div>
                     <div className='flex flex-col'>
-                        <label className='uppercase text-sm py-2'>Phone Number</label>
+                        <label className='uppercase text-sm py-2'>Phone</label>
                         <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='phone' placeholder='Enter your phone number'></input>
                     </div>
                 </div>
