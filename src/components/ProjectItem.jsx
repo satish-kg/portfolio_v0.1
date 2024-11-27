77532c90c7d219865c0f7585130f379e154d2b9c
Satish_KG <satishkrgupta555@gmail.com>
2024-11-28 00:54:32 +0530

    final style done

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const ProjectItem = ({img, title, console, url}) => {
     return (
         // <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
-        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-red-600 to-[#001b5e]">
             <img src={img} className="rounded-xl group-hover:opacity-10"></img>
-            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <div className="hidden group-hover:block absolute md:top-[50%] md:left-[50%] md:translate-x-[-50%] md:translate-y-[-50%]">
                 <h3 className="text-2xl text-white tracking-wider text-center font-bold">
                     {title}
                 </h3>
