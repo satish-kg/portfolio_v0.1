db413fea9a90d58cab2daa837adb514bc612554d
Satish Kr. <satishkrgupta555@gmail.com>
2025-09-29 14:26:44 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,10 +11,10 @@ const Projects = () => {
             <h1 className='text-4xl font-bold text-center text-[#001b5e] pb-5'>Projects</h1>
             <p className='text-center py-8'>Check out some of my most recent projects. Click <a href='https://github.com/satish-kg?tab=repositories' className='text-[#24479e]'>here</a> for more.</p>
             <div className='grid sm:grid-cols-2 gap-12'>
-                <ProjectItem img={placeholder1} title='Pizza Store' console= 'Next.js and MongoDB Atlas based application with CRUD functionality.' url="https://pizzastore-snowy.vercel.app/"/>
-                <ProjectItem img={placeholder2} title='Meme Maker' console='React Vite based application. User can enter text and download the meme.'  url="https://peppy-bunny-524ca6.netlify.app/"/>
+                <ProjectItem img={placeholder1} title='Pizza Store Full Stack' console= 'End-to-end full-stack Next.js and NodeJS-MongoDB based application with CRUD functionality.' url="https://pizzastore-snowy.vercel.app/"/>
+                <ProjectItem img={placeholder4} title='AI Sentiment Analyzer' console='End-to-end full-stack AI application to analyze positive/negative/neutral sentiment in a string. Dockerized python backend deployed on render. Frondend on netlify.'  url="https://senfiment-analyzer.netlify.app/"/>
                 <ProjectItem img={placeholder3} title='Excel to JSON' console='A MERN app, users can upload an Excel file and data is converted in JSON format and then visualized to compare easily.'  url="https://github.com/satish-kg/excel2json"/>
-                <ProjectItem img={placeholder4} title='Calculator' console='A super simple JavaScript calculator.'  url="https://vocal-narwhal-913661.netlify.app/"/>
+                <ProjectItem img={placeholder2} title='Meme Maker' console='React Vite based application. User can enter text and download the meme.'  url="https://peppy-bunny-524ca6.netlify.app/"/>
             </div>
         </div>
     )
